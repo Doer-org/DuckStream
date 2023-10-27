9b4ee08425178781046e844cd9a2cb85ae172ba0
RyushiAok <55625375+RyushiAok@users.noreply.github.com>
2023-10-28 01:44:48 +0900

    use fsharp 8.0

--- server/src/Api/Program.fs
+++ server/src/Api/Program.fs
@@ -7,9 +7,9 @@ let main args =
     let builder = WebApplication.CreateBuilder(args)
     let app = builder.Build()
 
-    app.MapGet("/", Func<string>(fun () -> "Hello World!")) |> ignore
+    app.MapGet("/", Func<string>(fun () -> "Hello World!"))
+    |> ignore
 
     app.Run()
 
     0 // Exit code
-
