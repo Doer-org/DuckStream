5e8150d84f091076cd11f66861e858f9de24beac
RyushiAok <55625375+RyushiAok@users.noreply.github.com>
2023-10-29 18:27:55 +0900

    remove terraform settings

--- terraform/gcp/duckstream/modules/iam/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "bucket_name" {
-  description = "The name of the Google Cloud Storage bucket"
-  type        = string
-}
-
-variable "service_account_email" {
-  description = "The email address of the service account"
-  type        = string
-}
--- terraform/gcp/duckstream/modules/strage/variable.tf
+++
@@ -1,9 +0,0 @@
-variable "bucket_name" {
-  description = "The name of the Google Cloud Storage bucket"
-  type        = string
-}
-
-variable "default_region" {
-  description = "The default region for the Google Cloud Storage bucket"
-  type        = string
-}
