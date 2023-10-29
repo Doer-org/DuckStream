51768a9c5d03e9c8944b07412251e805d21d1ffb
miso-devel <kengo279@icloud.com>
2023-10-30 02:49:46 +0900

    フォントの追加

--- client/font/font.ts
+++ client/font/font.ts
@@ -1,5 +1,10 @@
-import { Dela_Gothic_One } from 'next/font/google';
+import { Dela_Gothic_One, Zen_Maru_Gothic } from 'next/font/google';
 export const DelaGothic = Dela_Gothic_One({
   subsets: ['latin'],
   weight: '400',
+});
+
+export const ZenMaruGothic = Zen_Maru_Gothic({
+  subsets: ['latin'],
+  weight: '700',
 });
