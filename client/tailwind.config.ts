94a91271cfd3269a4aba2c12c01dcfef8ba4c341
miso-devel <kengo279@icloud.com>
2023-10-29 17:29:58 +0900

    tailwind setting

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
@@ -13,8 +13,12 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        main: '#F2C849',
+        secondary: '#02735E',
+      },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
