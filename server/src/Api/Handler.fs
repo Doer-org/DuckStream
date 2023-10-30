81b147cacf63479bcfc3df404bd3853b85b5da9d
RyushiAok <55625375+RyushiAok@users.noreply.github.com>
2023-10-31 00:11:23 +0900

    feat(server): add api handlers

---
+++ server/src/Api/Handler.fs
@@ -0,0 +1,84 @@
+module Api.Handler
+
+open Falco
+open Application.CommandHandler
+open System.Text.Json
+open FsToolkit.ErrorHandling
+open Domain.Types
+
+let errorHandler (isDev: bool) (e: CommandError) : HttpHandler =
+    if isDev then
+        Response.withStatusCode 500
+        >> Response.ofJson {| error = e |}
+    else
+        Response.withStatusCode 500
+        >> Response.ofJson {| error = "error!" |}
+
+let saveImage
+    (errorHandler: CommandError -> HttpHandler)
+    (repos: Repositories)
+    : HttpHandler =
+    fun ctx ->
+        task {
+            let! request =
+                Request.getJsonOptions<{| base64: string |}>
+                    JsonSerializerOptions.Default
+                    ctx
+
+            let ret = saveImage repos request.base64 |> Async.RunSynchronously
+
+            return
+                match ret with
+                | Ok image -> Response.ofJson {| data = image |} ctx
+                | Error e -> errorHandler e ctx
+        }
+
+let getImage
+    (errorHandler: CommandError -> HttpHandler)
+    (repos: Repositories)
+    : HttpHandler =
+    fun ctx ->
+        task {
+            let route = Request.getRoute ctx
+            let id: Id = route.Get("id")
+
+            let ret = getImage repos id |> Async.RunSynchronously
+
+            return
+                match ret with
+                | Ok image -> Response.ofJson {| data = image |} ctx
+                | Error e -> errorHandler e ctx
+        }
+
+let healthML
+    (errorHandler: CommandError -> HttpHandler)
+    (repos: Repositories)
+    : HttpHandler =
+    fun ctx ->
+        task {
+            let ret = healthML repos |> Async.RunSynchronously
+
+            return
+                match ret with
+                | Ok resp -> Response.ofJson {| data = resp |} ctx
+                | Error e -> errorHandler e ctx
+        }
+
+let inference
+    (errorHandler: CommandError -> HttpHandler)
+    (repos: Repositories)
+    : HttpHandler =
+    fun ctx ->
+        task {
+            let! request =
+                Request.getJsonOptions<InferenceRequest>
+                    JsonSerializerOptions.Default
+                    ctx
+
+            let ret = inference repos request |> Async.RunSynchronously
+
+            return
+                match ret with
+                | Ok resp -> Response.ofJson {| data = resp |} ctx
+                | Error e -> errorHandler e ctx
+        }
--- server/src/Api/Program.fs
+++ server/src/Api/Program.fs
@@ -1,28 +1,42 @@
 open Falco
 open Falco.Routing
 open Falco.HostBuilder
+open Application.CommandHandler
 
 [<EntryPoint>]
-let main args =
+let main _ =
+    let errorHandler = Api.Handler.errorHandler true
+
+    let repos = {
+        duckstreamImage =
+            Repository.Database.duckstreamRepo {
+                IS_DEV = true
+                DB_HOST = " "
+                DB_USER = " "
+                DB_PASSWORD = " "
+                DB_DATABASE = " "
+            }
+        GCStorage =
+            Repository.GCS.gcsStore {
+                GCP_CREDENTIALS = " "
+                GCP_BUCKET_NAME = " "
+                GCS_URL = " "
+            }
+        mlService =
+            Repository.ML.mlRepo {
+                ML_URL = " "
+                timeout = System.TimeSpan.FromSeconds(30.0)
+            }
+    }
+
     webHost [||] {
         endpoints [
-            get "/health" (Response.ofJson {| env = "local" |})
-            get "/health/ml" (Response.ofJson {| env = "local" |})
-
-            get "/image/{id}" (Response.ofJson {| id = "1"; img = "base64" |})
-
-            (*
-                input: { parent_image: base64 }
-            *)
-            post
-                "/inference"
-                (Response.ofJson {|
-                    parent_id = "1"
-                    child_id = "1"
-                    img = "base64"
-                |})
-
+            get "/health" (Response.ofJson {| env = " " |})
+            get "/health/ml" (Api.Handler.healthML errorHandler repos)
+            post "/image" (Api.Handler.saveImage errorHandler repos)
+            get "/image/{id}" (Api.Handler.getImage errorHandler repos)
+            post "/inference/{id}" (Api.Handler.inference errorHandler repos)
         ]
     }
 
-    0 // Exit code
+    0
