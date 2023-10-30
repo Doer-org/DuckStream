fbcc82a8cab72341f45d6523339cd38fe56425d2
miso-devel <kengo279@icloud.com>
2023-10-30 21:27:33 +0900

    fix duplicate html tag

--- client/src/app/draw/layout.tsx
+++ client/src/app/draw/layout.tsx
@@ -8,15 +8,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang='ja'>
-      <body>
-        <header className='h-[8vh] flex items-center bg-secondary'>
-          <h1 className='text-sm text-main w-[95vw] m-auto'>Duck Stream</h1>
-        </header>
-        <main className='flex min-h-[92vh] flex-col items-center justify-center gap-5 bg-secondary'>
-          {children}
-        </main>
-      </body>
-    </html>
+    <>
+      <header className='h-[8vh] flex items-center bg-secondary'>
+        <h1 className='text-sm text-main w-[95vw] m-auto'>Duck Stream</h1>
+      </header>
+      <main className='flex min-h-[92vh] flex-col items-center justify-center gap-5 bg-secondary'>
+        {children}
+      </main>
+    </>
   );
 }
