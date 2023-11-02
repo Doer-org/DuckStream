d5e5ffd3affa1ceaf09c375659e7cf1148d2ae86
RyushiAok <55625375+RyushiAok@users.noreply.github.com>
2023-11-02 19:33:59 +0900

    feat(server): add mock database

--- server/src/Api/Repositories/Mock.fs
+++ server/src/Api/Repositories/Mock.fs
@@ -4,7 +4,7 @@ open System
 
 let mockGCS: Application.Persistence.GCStorageRepo = {
     upload =
-        fun base64 ->
+        fun _ ->
             async {
                 let resp: Domain.Types.Image = {
                     id = Guid.NewGuid().ToString()
@@ -14,7 +14,7 @@ let mockGCS: Application.Persistence.GCStorageRepo = {
                 return resp |> Ok
             }
     downloadBase64 =
-        fun id ->
+        fun _ ->
             async {
                 let resp: Domain.Types.Base64 = "base64"
                 return resp |> Ok
@@ -45,3 +45,111 @@ let mockMLService: Application.Infra.MLService = {
                 return resp |> Ok
             }
 }
+
+open Database.Table
+open Application.Persistence
+open Domain.Types
+
+let mockDB: Application.Persistence.DuckStreamImageRepo =
+    let mutable images: Database.Table.gcs_image list = []
+    let mutable inference_result: Database.Table.inference_result list = []
+
+    {
+        saveImage =
+            fun _ ->
+                async {
+                    let new_row = {
+                        img_id = Guid.NewGuid().ToString()
+                        img_url = "img_url"
+                        created_at = DateTime.Now
+                    }
+
+                    images <- new_row :: images
+
+                    let resp: Domain.Types.Image = {
+                        id = new_row.img_id
+                        url = new_row.img_url
+                    }
+
+
+                    return resp |> Ok
+                }
+        getImage =
+            fun id ->
+                async {
+                    let resp =
+                        images
+                        |> List.tryFind (fun row -> row.img_id = id)
+                        |> function
+                            | Some row ->
+                                let resp: Domain.Types.Image = {
+                                    id = row.img_id
+                                    url = row.img_url
+                                }
+
+                                resp |> Ok
+                            | None -> Error(PersistenceError.DB "not found")
+
+                    return resp
+                }
+        getInferenceResult =
+            fun id ->
+                async {
+                    let resp =
+                        query {
+                            for result in inference_result do
+                                join image in images
+                                                  on
+                                                  (result.result_img_id = image.img_id)
+
+                                join img in images on (result.input_img_id = img.img_id)
+                                where (result.input_img_id = id)
+
+                                select {|
+                                    input_img_id = result.input_img_id
+                                    input_img_url = img.img_url
+                                    result_img_id = result.result_img_id
+                                    result_img_url = image.img_url
+                                    prompt = result.prompt
+                                    converted_prompt = result.converted_prompt
+                                |}
+
+                                take 1
+                        }
+                        |> Seq.tryHead
+                        |> function
+                            | None -> Error(PersistenceError.DB "not found")
+                            | Some row ->
+                                let r: InferenceResult = {
+                                    input_image = {
+                                        id = row.input_img_id
+                                        url = row.input_img_url
+                                    }
+                                    result_image = {
+                                        id = row.result_img_id
+                                        url = row.result_img_url
+                                    }
+                                    prompt = row.prompt
+                                    converted_prompt = row.converted_prompt
+                                }
+
+                                r |> Ok
+
+
+                    return resp
+                }
+        registerInferenceResult =
+            fun result ->
+                async {
+                    inference_result <-
+                        {
+                            input_img_id = result.input_image.id
+                            result_img_id = result.result_image.id
+                            prompt = result.prompt
+                            converted_prompt = result.converted_prompt
+                        }
+                        :: inference_result
+
+                    return Ok()
+                }
+    }
