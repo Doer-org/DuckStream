7ad717c1df79e83d89aeb8a4c736d7d3fb3a74ea
RyushiAok <55625375+RyushiAok@users.noreply.github.com>
2023-11-02 19:35:48 +0900

    chore(server): mock環境対応

--- server/src/Api/Program.fs
+++ server/src/Api/Program.fs
@@ -8,7 +8,6 @@ open DotNetEnv
 
 type Environment =
     | Local
-    | Mock
     | Prod
 
 type AppEnv = {
@@ -17,6 +16,7 @@ type AppEnv = {
     db: Repository.Database.DBEnv
     gcs: Repository.GCS.GCS_ENV
     ml: Repository.ML.MLEnv
+    is_mock: {| ml: bool; db: bool; gcs: bool |}
 }
 
 let env =
@@ -28,14 +28,19 @@ let env =
 
     let ENVIRONMENT =
         match Env.GetString("ENVIRONMENT") with
-        | "local" -> Local
-        | "mock" -> Mock
-        | _ -> Prod
+        | "production" -> Prod
+        | _ -> Local
+
+    let is_mock = {|
+        ml = Env.GetBool("MOCK_ML")
+        db = Env.GetBool("MOCK_DB")
+        gcs = Env.GetBool("MOCK_GCS")
+    |}
 
     {
         environment = ENVIRONMENT
         db = {
-            IS_DEV = ENVIRONMENT <> Prod
+            IS_PROD = (not is_mock.db) && (ENVIRONMENT = Prod)
             DB_HOST = Env.GetString("DB_HOST")
             DB_PORT = Env.GetInt("DB_PORT")
             DB_USER = Env.GetString("DB_USER")
@@ -52,6 +57,7 @@ let env =
             TIMEOUT = TimeSpan.FromSeconds(Env.GetDouble("ML_REQUEST_TIMEOUT_SEC"))
         }
         client_url = Env.GetString("CLIENT_URL")
+        is_mock = is_mock
     }
 
 type Program =
@@ -63,24 +69,46 @@ type Program =
 let main _ =
     let errorHandler = Api.Handler.errorHandler (env.environment <> Prod)
 
-    let repos =
-        if env.environment = Mock then
-            {
-                duckstreamImage = Repository.Database.duckstreamRepo env.db
-                GCStorage = Repository.Mock.mockGCS
-                mlService = Repository.Mock.mockMLService
-            }
-        else
-            {
-                duckstreamImage = Repository.Database.duckstreamRepo env.db
-                GCStorage = Repository.GCS.gcsStore env.gcs
-                mlService = Repository.ML.mlRepo env.ml
-            }
+    let repos = {
+        duckstreamImage =
+            if env.is_mock.db then
+                Repository.Mock.mockDB
+            else
+                Repository.Database.duckstreamRepo env.db
+
+        GCStorage =
+            if env.is_mock.db then
+                Repository.Mock.mockGCS
+            else
+                Repository.GCS.gcsStore env.gcs
+
+
+        mlService =
+            if env.is_mock.ml then
+                Repository.Mock.mockMLService
+            else
+                Repository.ML.mlRepo env.ml
+    }
+
+
+    let healthHandler =
+        Response.ofJson {|
+            db = {|
+                is_mock = env.is_mock.db
+                is_prod = env.db.IS_PROD
+            |}
+            gcs = {| is_mock = env.is_mock.gcs |}
+            ml = {|
+                is_mock = env.is_mock.ml
+                time_out = env.ml.TIMEOUT
+            |}
+            client_url = env.client_url
+        |}
 
     webHost [||] {
         endpoints [
             get "/" (Response.ofJson {| hello = "world" |})
-            get "/health" (Response.ofJson {| env = env |})
+            get "/health" healthHandler
             get "/health/ml" (Api.Handler.healthML errorHandler repos)
             post "/image" (Api.Handler.saveImage errorHandler repos)
             get "/image/{id}" (Api.Handler.getImage errorHandler repos)
