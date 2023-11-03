1114d4dbfbae0ebea0f68ca89465dfc518c399b4
ryoya0902 <ryuasa1192@gmail.com>
2023-11-03 21:05:32 +0900

    fix(ml):modalのデプロイを修正

--- ml/src/main.py
+++ ml/src/main.py
@@ -1,77 +1,87 @@
-import base64
-import io
-import os
-
-import modal
-import uvicorn
-from dotenv import load_dotenv
-from fastapi import FastAPI
-from modal import asgi_app
-from omegaconf import OmegaConf
-from PIL import Image
-from starlette.middleware.cors import CORSMiddleware
-
-from .configs import DiffusionConfig, MorphotoConfig
+import subprocess
+from modal import Image, Secret, Stub, asgi_app, method, gpu
 from .models import InferenceRequest
-from .morphoto import Morphoto
 
-morphoto_config = OmegaConf.create(MorphotoConfig)
-morphoto = Morphoto(morphoto_config)
-app = FastAPI()
+def download_models():
+    import torch
+    from diffusers import StableDiffusionImg2ImgPipeline
+    pipe = StableDiffusionImg2ImgPipeline.from_pretrained("runwayml/stable-diffusion-v1-5", torch_dtype=torch.float16, revision="fp16")
+    pipe.save_pretrained("data/stable-diffusion-v1-5")
+    subprocess.run(['git', 'clone', "https://github.com/MosasoM/inappropriate-words-ja.git", "data/inappropriate-words-ja"])
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["http://localhost:3000", "https://api.morphoto.app"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+image = (
+    Image.debian_slim()
+    .apt_install("git")
+    .poetry_install_from_file(poetry_pyproject_toml="pyproject.toml", poetry_lockfile="poetry.lock")
+    .run_function(download_models)
 )
 
-stub = modal.Stub("mophoto-fastapi")
-modal_image = modal.Image.debian_slim().poetry_install_from_file(
-    poetry_pyproject_toml="pyproject.toml", poetry_lockfile="poetry.lock"
-)
+stub = Stub("duckstream-fastapi", image=image)
+@stub.cls(gpu=gpu.T4(),container_idle_timeout=240)
+class Model:
+    def __enter__(self):
+        from .morphoto import Morphoto
+        from .configs import MorphotoConfig
+        from omegaconf import OmegaConf
 
+        morphoto_config = OmegaConf.create(MorphotoConfig)
+        self.morphoto = Morphoto(morphoto_config)
 
-@app.post("/inference")
-def inference(request: InferenceRequest) -> dict[str, str]:
-    if request.is_mock:
-        return {"converted_image": request.image, "prompt": request.prompt}
-    image = io.BytesIO(base64.b64decode(request.image))
-    image = Image.open(image)
-    converted_image, prompt = morphoto.convert(request.prompt, image, request.strength)
+    @method()
+    def inference(self, request: InferenceRequest):
+        if request.is_mock:
+            return {"converted_image": request.image, "prompt": request.prompt}
 
-    buffered = io.BytesIO()
-    converted_image.save(buffered, format="PNG")
-    converted_image = base64.b64encode(buffered.getvalue()).decode()
-    result = {"converted_image": converted_image, "prompt": prompt}
-    return result
+        from PIL import Image
+        import io
+        import base64
 
+        image = io.BytesIO(base64.b64decode(request.image))
+        image = Image.open(image)
+        converted_image, prompt = self.morphoto.convert(request.prompt, image, request.strength)
 
-@app.get("/health")
-def health() -> dict[str, str]:
-    return {"status": "ok", "device": DiffusionConfig.device}
-
-
-load_dotenv()
-data_dir = os.getenv("DATA_DIR") or "data"
-
-
-@stub.function(
-    image=modal_image,
-    mounts=[modal.Mount.from_local_dir(data_dir, remote_path="/root/data")],
-    secret=modal.Secret.from_name("morphoto-ml-secrets"),
-    gpu="T4",
-)
-@asgi_app()
-def fastapi_app() -> FastAPI:
-    return app
-
+        buffered = io.BytesIO()
+        converted_image.save(buffered, format="PNG")
+        converted_image = base64.b64encode(buffered.getvalue()).decode()
+        result = {"converted_image": converted_image, "prompt": prompt}
+        return result
 
 @stub.local_entrypoint()
-def main() -> None:
+def main():
+    import uvicorn
+    from fastapi import FastAPI
+    app = FastAPI()
+
+    model_instance = Model()
+    @app.post("/inference")
+    def inference(request: InferenceRequest):
+        return model_instance.inference.remote(request)
+    @app.get("/health")
+    def health() -> dict[str, str]:
+        from .configs import DiffusionConfig
+        return {"status": "ok", "device": DiffusionConfig.device}
     uvicorn.run(app, host="0.0.0.0", port=8000)
 
+@stub.function(
+    container_idle_timeout=300,
+    secret=Secret.from_name("duckstream-ml-secrets"),
+)
+@asgi_app()
+def app():
+    from fastapi import FastAPI
+    from fastapi.middleware.cors import CORSMiddleware
+    web_app = FastAPI()
+
+    @web_app.post("/inference")
+    async def inference(request: InferenceRequest):
+        return Model().inference.remote(request)
+
+    @web_app.get("/health")
+    async def health():
+        from .configs import DiffusionConfig
+        return {"status": "ok", "device": DiffusionConfig.device}
+
+    return web_app
 
 if __name__ == "__main__":
     main()
