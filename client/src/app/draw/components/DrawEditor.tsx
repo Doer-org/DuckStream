0a7a493e416a8212cc9fab619134d57002baf9f4
miso-devel <kengo279@icloud.com>
2023-11-04 03:25:06 +0900

    create editor

--- client/src/app/draw/components/DrawEditor.tsx
+++ client/src/app/draw/components/DrawEditor.tsx
@@ -1,52 +1,74 @@
-import { TActionState, TDrawCondition, TLines, TPicture } from '@/types/app';
-import { FC, useEffect, useState } from 'react';
-
+"use clients";
+import { TActionState, TDrawCondition, TPicture, TPoints } from "@/types/app";
+import { toInitialXY, toXY } from "@/utils/position";
+import { FC, useEffect, useState } from "react";
+import KonvasEditor from "./KonvasEditor";
 type TPictureEditorProps = {
   pictureState: TActionState<TPicture>;
   drawConditionState: TActionState<TDrawCondition>;
 };
 
-export const DrawEditor: FC<TPictureEditorProps> = ({
-  pictureState,
-  drawConditionState,
-}) => {
-  const [lines, setLines] = useState<TLines>([[0, 0]]);
-  // TODO: Editor内のタップとかで切り替えられるようにしたい
+// https://github.com/konvajs/react-konva#usage-with-nextjs の例通りでもできるが、フォントが合わなくなるので普通にimportしてる
+// const KonvasEditor = dynamic(() => import("./KonvasEditor"), { ssr: false });
+
+export const DrawEditor: FC<TPictureEditorProps> = ({ pictureState, drawConditionState }) => {
+  const [points, setPoints] = useState<TPoints>([]);
+  // 別に取らなくても良さそう。
+  const [initialPoints, setInitialPoints] = useState<TPoints>([]);
+
   const onSwitchCondition = () => {
-    if (drawConditionState.state === 'DRAWING') {
-      drawConditionState.setState('MOVING');
-      pictureState.setState([...pictureState.state, lines]);
-      setLines([]);
-    } else if (drawConditionState.state === 'MOVING') {
-      drawConditionState.setState('DRAWING');
+    if (drawConditionState.state === "DRAWING") {
+      drawConditionState.setState("MOVING");
+      // MOVINGになる段階で一度線画を保存して、現在の状態を初期化する
+      pictureState.setState([...pictureState.state, points]);
+      setPoints([]);
+    } else {
+      drawConditionState.setState("DRAWING");
+      // 現在地を初期値にする
+      navigator.geolocation.getCurrentPosition((position) => setPoints([toXY(position)]));
     }
   };
-  useEffect(() => {
-    if (drawConditionState.state === 'DRAWING') {
-      navigator.geolocation.watchPosition((position) => {
-        setLines([
-          ...lines,
-          [position.coords.latitude, position.coords.longitude],
-        ]);
-      });
-    }
-  }, [lines, drawConditionState.state]);
-  console.log(lines);
-  console.log(drawConditionState.state);
-  // TODO: 初期値は選択できるようにしておきたい
 
-  // TODO:　pixelsに線の描画が終わったらpushしていく
+  // 初期値を入れる
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      // viewに表す際に何かしらの尺度に置き換えておきたい
+      // というのも今いるところだと [135.123456... , 35.123456...] とかで移動しても小数点のところがちょっと動くだけで線画としてはうまく表示されない
+      // なので、もうちょっと1~300くらいで移動できるように調整したい
+      setPoints([toXY(position)]);
+      setInitialPoints([toInitialXY(position)]);
+    });
+  }, []);
+
+  useEffect(() => {
+    // DRAWING以外の状態では線画を作らないようにしている
+    if (drawConditionState.state === "DRAWING" && points.length > 1) {
+      navigator.geolocation.watchPosition((position) => setPoints([...points, toXY(position)]));
+    }
+  }, [points, drawConditionState]);
+
   return (
     <>
       <div>
-        <div>lines:{lines.toString()}</div>
-        <div>pictures:{pictureState.state.toString()}</div>
+        <div className="break-all">
+          <p>points</p>
+          {points.map((point, i) => {
+            return <div key={i}>{point.toString()}</div>;
+          })}
+        </div>
+        <div className="break-all">
+          <p>pictures</p>
+          {pictureState.state.map((picture, i) => {
+            return <div key={i}>{picture.toString()}</div>;
+          })}
+        </div>
       </div>
-      <div
-        className='bg-white w-[95vw] h-[70vh] flex items-center justify-center rounded-sm'
-        onClick={onSwitchCondition}
-      >
-        <div className='w-2 h-2 rounded-full bg-secondary' />
+      <div className="bg-white max-w-[95vw] mx-auto rounded-md overflow-hidden" onClick={onSwitchCondition}>
+        {/*  */}
+        <KonvasEditor
+          pictureState={{ state: pictureState.state, setState: pictureState.setState }}
+          pointsState={{ state: points, setState: setPoints }}
+        />
       </div>
     </>
   );
---
+++ client/src/app/draw/components/KonvasEditor.tsx
@@ -0,0 +1,135 @@
+"use client";
+import { TActionState, TPicture, TPoints } from "@/types/app";
+import Konva from "konva";
+import { FC, useRef, useState } from "react";
+import { Layer, Line, Path, Stage, type KonvaNodeEvents } from "react-konva";
+
+type TPointer = { x: number; y: number };
+type TKonvasEditorProps = {
+  pictureState: TActionState<TPicture>;
+  pointsState: TActionState<TPoints>;
+};
+
+const KonvasEditor: FC<TKonvasEditorProps> = ({ pictureState, pointsState }) => {
+  Konva.hitOnDragEnabled = true;
+  const stageRef = useRef<Konva.Stage>(null);
+  const layerRef = useRef<Konva.Layer>(null);
+  const lineRef = useRef<Konva.Line>(null);
+  const [lastDist, setLastDist] = useState(0);
+  const [dist, setDist] = useState(0);
+  const [lastCenter, setLastCenter] = useState<null | TPointer>(null);
+  const [newCenter, setNewCenter] = useState<null | TPointer>(null);
+  const [isDragging, setIsDragging] = useState(false);
+  const [scale, setScale] = useState(1);
+
+  const getCenter = (p1: TPointer, p2: TPointer): TPointer => ({ x: (p1.x + p2.x) / 2, y: (p1.y + p2.y) / 2 });
+  const getDistance = (p1: TPointer, p2: TPointer) => Math.sqrt(Math.pow(p2.x - p1.x, 2) + Math.pow(p2.y - p1.y, 2));
+
+  // 現在はうまく動いていないが、将来的にピンチで拡大縮小ができるようになりたい
+  const onTouchMove: KonvaNodeEvents["onTouchMove"] = (event) => {
+    event.evt.preventDefault();
+
+    const t1 = event.evt.touches[0];
+    const t2 = event.evt.touches[1];
+
+    if (!stageRef.current) return;
+
+    if (t1 && !t2 && !stageRef.current.isDragging() && isDragging) {
+      stageRef.current.startDrag();
+      setIsDragging(false);
+    }
+
+    if (t1 && t2) {
+      if (stageRef.current.isDragging()) {
+        setIsDragging(true);
+        stageRef.current.stopDrag();
+      }
+
+      const p1 = { x: t1.clientX, y: t2.clientY };
+      const p2 = { x: t1.clientX, y: t2.clientY };
+
+      if (!lastCenter) {
+        setLastCenter(getCenter(p1, p2));
+        return;
+      }
+      setNewCenter(getCenter(p1, p2));
+
+      setDist(getDistance(p1, p2));
+
+      if (!lastDist) setLastDist(dist);
+      if (!newCenter) return;
+
+      const pointTo = {
+        x: (newCenter.x - stageRef.current.x()) / stageRef.current.scaleX(),
+        y: (newCenter.y - stageRef.current.y()) / stageRef.current.scaleX(),
+      };
+
+      // stageRef.current.scaleX()とかがちゃんと取れていない雰囲気がある
+      setScale(stageRef.current.scaleX() * (dist / lastDist));
+
+      stageRef.current.scaleX(scale);
+      stageRef.current.scaleY(scale);
+
+      setLastCenter(getCenter(p1, p2));
+      const dx = newCenter.x - lastCenter.x;
+      const dy = newCenter.y - lastCenter.y;
+
+      const newPos = {
+        x: newCenter.x - pointTo.x * scale + dx,
+        y: newCenter.y - pointTo.y * scale + dy,
+      };
+
+      stageRef.current.position(newPos);
+
+      setLastDist(dist);
+      setLastCenter(newCenter);
+    }
+
+    if (!layerRef.current || !lineRef.current) {
+      return;
+    }
+
+    layerRef.current.add(lineRef.current);
+    stageRef.current.add(layerRef.current);
+  };
+
+  const onTouchEnd: KonvaNodeEvents["onTouchEnd"] = (event) => {
+    const t1 = event.evt.touches[0];
+    const t2 = event.evt.touches[1];
+    if (t1 && t2) {
+      setLastDist(0);
+      setLastCenter(null);
+    }
+  };
+
+  return (
+    <Stage height={320} width={320} draggable onTouchMove={onTouchMove} onTouchEnd={onTouchEnd} ref={stageRef}>
+      <Layer ref={layerRef}>
+        {pictureState.state.map((points, i) => {
+          return (
+            <Line
+              key={i}
+              strokeWidth={4}
+              lineCap="round"
+              lineJoin="round"
+              stroke="black"
+              points={[...(points.flat(Infinity) as number[]), 10, 10]}
+            />
+          );
+        })}
+
+        <Line
+          strokeWidth={4}
+          lineCap="round"
+          lineJoin="round"
+          stroke="black"
+          points={[...(pointsState.state.flat(Infinity) as number[])]}
+          ref={lineRef}
+        />
+        <Path />
+      </Layer>
+    </Stage>
+  );
+};
+
+export default KonvasEditor;
