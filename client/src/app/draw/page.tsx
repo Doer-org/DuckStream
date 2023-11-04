908f558e851bc1eb5831143c4f41b3109b9ade7c
miso-devel <kengo279@icloud.com>
2023-11-04 16:36:29 +0900

    remove picture and condition

--- client/src/app/draw/page.tsx
+++ client/src/app/draw/page.tsx
@@ -1,14 +1,12 @@
 "use client";
-import { TDrawCondition, TPicture } from "@/types/app";
+import { TPoints } from "@/types/app";
 import { useState } from "react";
 import { ZenMaruGothic } from "../../../font/font";
-import { DrawCondition } from "./components/DrawCondition";
 import { DrawEditor } from "./components/DrawEditor";
 
 export default function Draw() {
   const [title, setTitle] = useState("");
-  const [picture, setPicture] = useState<TPicture>([]);
-  const [drawCondition, setDrawCondition] = useState<TDrawCondition>("DRAWING");
+  const [points, setPoints] = useState<TPoints>([]);
 
   const onSubmit = () => {
     // TODO: サーバー側に情報を渡す
@@ -30,23 +28,8 @@ export default function Draw() {
             保存
           </button>
         </div>
-
-        <div className="flex gap-1 items-center ms-auto">
-          <DrawCondition
-            drawConditionState={{
-              state: drawCondition,
-              setState: setDrawCondition,
-            }}
-          />
-        </div>
       </div>
-      <DrawEditor
-        pictureState={{ state: picture, setState: setPicture }}
-        drawConditionState={{
-          state: drawCondition,
-          setState: setDrawCondition,
-        }}
-      />
+      <DrawEditor pointsState={{ state: points, setState: setPoints }} />
       <div>
         <p className="text-sm">キャンバス内のクリックで描画/移動が切り替えれます</p>
       </div>
