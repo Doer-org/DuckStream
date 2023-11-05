bed4f1dd65dbe67f67fe9ff5a4151e3c2c19d809
miso-devel <kengo279@icloud.com>
2023-11-05 14:16:47 +0900

    config change

--- client/next.config.js
+++ client/next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    domains: ["storage.googleapis.com"],
+  },
   webpack: (config) => {
     config.externals = [...config.externals, { canvas: "canvas" }]; // required to make Konva & react-konva work
     return config;
