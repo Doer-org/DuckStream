d40c146670b1d4065a68fa132843f441e825b382
miso-devel <kengo279@icloud.com>
2023-11-05 14:17:58 +0900

    add result page

--- client/src/app/result/[id]/page.tsx
+++ client/src/app/result/[id]/page.tsx
@@ -1,13 +1,15 @@
-export default function Result({ params }: { params: { id: string } }) {
+import Image from "next/image";
+export default async function Result({ params }: { params: { id: string } }) {
   const { id } = params;
-  // TODO: ここでid使ってfetchしてくる
+  const image: { data: { url: string } } = await (await fetch(`${process.env.SERVER_URL}/image/${id}`)).json();
+
   return (
-    <main className='flex min-h-screen flex-col items-center justify-center gap-5 bg-secondary'>
-      <h1 className='font-bold text-4xl text-main stroke-black text-stroke'>
-        DUCK STREAM
-      </h1>
-      <div className='w-[95vw] h-[65vh] bg-slate-300 rounded-sm' />
-      <a className='text-2xl hover:text-main' href='/'>
+    <main className="flex min-h-screen flex-col items-center justify-center gap-5 bg-secondary">
+      <h1 className="font-bold text-4xl text-main stroke-black text-stroke">DUCK STREAM</h1>
+      <div className="p-5">
+        <Image src={image.data.url} width={300} height={300} alt="result-image" />
+      </div>
+      <a className="text-2xl hover:text-main" href="/">
         TOPへ
       </a>
     </main>
